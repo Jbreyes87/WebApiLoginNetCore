512c8097dc6c7633596eb7ec3a8e97168b8fe4a2
johnrl <Jreyes_25@outlook.es>
2021-03-22 14:09:44 -0500

    proyecto estable

---
+++ README.md
@@ -0,0 +1,6 @@
+# ERP Core Services API
+Este repositorio contiene el proyecto que se encargará de la lógica base del ERP.
+
+* **Tecnología**: .Net Core
+* **Framework**: Web API
+* **Base de datos**: SQL
---
+++ WebApiLogin/Contexto/AplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiLogin.Modelo;
+
+namespace WebApiLogin.Contexto
+{
+    public class AplicationDbContext : IdentityDbContext<AplicationUser>
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options)
+        :base(options){
+
+            }
+
+            // puedes seguir utilizando tus modelos para generar tus tablas de manera normal
+            //ya que el IdentityDbContext es una interface que usa todas sus clses como el dbcontext etc
+    }
+}
